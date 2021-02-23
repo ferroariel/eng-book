7c63389a49a7e584015a1243b4bcb4345a7b73fe
Ariel Ferro <aferro@sparkdigital.com>
2021-02-23 09:55:46 -0300

    accesible navigation

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav aria-label="Navegacion principal">
+            <ul>
+                <li><a href="https://google.com">Link 1</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
